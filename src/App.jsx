f781fd1387de7567a8abfc5baf24a061410bf686
Niwantha Sanjula <niwanthasanjulaofficial@gmail.com>
2025-09-19 12:57:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from './components/Navbar';
+
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+ return (
+	<main>
+		<Navbar/>
+
+	</main>
+ )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+	<div className='font-modern-negra flex-center text-6xl text-gradient'>
+	  <h1>HI</h1>
+	  <h1>HI</h1>
+	  <h1>HI</h1>
+	</div>
+  )
+}
+
+export default Navbar
