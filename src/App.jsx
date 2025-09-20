91c18454dcbca555a597bde205813d24b13965bd
Niwantha Sanjula <niwanthasanjulaofficial@gmail.com>
2025-09-20 10:36:14 +0530

    Implemented art section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
+import Art from './components/Art';
 
 
 
@@ -17,6 +18,7 @@ const App = () => {
 		<Hero/>
 		<Cocktails/>
 		<About/>
+		<Art/>
 
 		
 
